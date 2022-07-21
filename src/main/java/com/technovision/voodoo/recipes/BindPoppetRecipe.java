eb20f450838f38d7ca1a6344dd96c9241105b4c6
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-07-20 23:27:12 -0700

    Fix bug with bind poppet recipe

--- src/main/java/com/technovision/voodoo/recipes/BindPoppetRecipe.java
+++ src/main/java/com/technovision/voodoo/recipes/BindPoppetRecipe.java
@@ -10,8 +10,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialCraftingRecipe;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Custom special recipe that binds a taglock to a poppet, passing NBT tag data.
@@ -24,11 +26,9 @@ public class BindPoppetRecipe extends SpecialCraftingRecipe {
         super(id);
     }
 
-    @Override
-    public DefaultedList<ItemStack> getRemainder(CraftingInventory inventory) {
-        DefaultedList<ItemStack> itemStacks = DefaultedList.of();
-        int size = inventory.getHeight() * inventory.getWidth();
-        for (int i = 0; i < size; ++i) {
+    private List<ItemStack> getItems(CraftingInventory inventory) {
+        List<ItemStack> itemStacks = new ArrayList<>();
+        for (int i = 0; i < inventory.size(); ++i) {
             ItemStack itemStack = inventory.getStack(i);
             if (!itemStack.isEmpty()) {
                 itemStacks.add(itemStack);
@@ -39,7 +39,7 @@ public class BindPoppetRecipe extends SpecialCraftingRecipe {
 
     @Override
     public boolean matches(CraftingInventory inventory, World world) {
-        DefaultedList<ItemStack> itemStacks = getRemainder(inventory);
+        List<ItemStack> itemStacks = getItems(inventory);
         if (itemStacks.size() != 2) return false;
         ItemStack itemStack1 = itemStacks.get(0);
         ItemStack itemStack2 = itemStacks.get(1);
@@ -57,7 +57,7 @@ public class BindPoppetRecipe extends SpecialCraftingRecipe {
 
     @Override
     public ItemStack craft(CraftingInventory inventory) {
-        DefaultedList<ItemStack> itemStacks = getRemainder(inventory);
+        List<ItemStack> itemStacks = getItems(inventory);
         ItemStack itemStack1 = itemStacks.get(0);
         ItemStack itemStack2 = itemStacks.get(1);
         if (itemStack2.getItem() == ModItems.TAGLOCK_KIT) {
